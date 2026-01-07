3697457ffd80efda6e05df21c88b31599e063318
idtsitsharsh <harshg.prsnl@gmail.com>
2026-01-07 11:23:30 +0530

    Updating Middlewar.js #5

--- middleware.js
+++ middleware.js
@@ -2,7 +2,6 @@ import { NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
 export function middleware(req) {
-  // fallback to header if req.cookies.get() fails
   const token =
     req.cookies.get("token")?.value ||
     req.headers.get("cookie")?.split("token=")?.[1]?.split(";")[0];
@@ -23,5 +22,5 @@ export function middleware(req) {
 
 
 export const config = {
-  matcher: ["/dashboard/:path*"], // ✅ ONLY pages
+  matcher: [],
 };
